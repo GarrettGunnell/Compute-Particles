95b8879b676c8d17a39361fafd18e1e874605879
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-01-19 02:59:12 -0800

    Create buffer of particles and their positions

--- Assets/Particles.cs
+++ Assets/Particles.cs
@@ -4,4 +4,40 @@ using UnityEngine;
 
 public class Particles : MonoBehaviour {
 
+    private struct Particle {
+        public Vector3 position;
+    }
+
+    private int PARTICLE_SIZE = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Particle));
+
+    public int particleCount = 1000;
+    public Material particleMaterial;
+
+    private Particle[] particles;
+    private ComputeBuffer particleBuffer;
+
+    private void Start() {
+        particles = new Particle[particleCount];
+        
+        for (int i = 0; i < particleCount; ++i) {
+            particles[i].position = new Vector3(Random.Range(-1.0f, 1.0f), 
+                                                Random.Range(-1.0f, 1.0f), 
+                                                Random.Range(-1.0f, 1.0f));
+        }
+
+        particleBuffer = new ComputeBuffer(particleCount, PARTICLE_SIZE);
+        particleBuffer.SetData(particles);
+
+        particleMaterial.SetBuffer("particleBuffer", particleBuffer);
+    }
+
+    private void OnDestroy() {
+        if (particleBuffer != null) particleBuffer.Release();
+    }
+
+    private void OnRenderObject() {
+        particleMaterial.SetPass(0);
+        Graphics.DrawProceduralNow(MeshTopology.Points, 1, particleCount);
+    }
+
 }
