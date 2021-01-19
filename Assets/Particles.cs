4fcbf77556b36afe2f77286010d2c161e51037c4
Garrett Gunnell <gunnellg@oregonstate.edu>
2021-01-19 04:55:38 -0800

    Initialize particles as grid

--- Assets/Particles.cs
+++ Assets/Particles.cs
@@ -11,8 +11,9 @@ public class Particles : MonoBehaviour {
     private int PARTICLE_SIZE = System.Runtime.InteropServices.Marshal.SizeOf(typeof(Particle));
 
     public int particleCount = 1000;
+    private int dimension;
     
-    [Range(1, 15)]
+    [Range(1, 10000)]
     public float radius = 1.0f;
 
     public Material particleMaterial;
@@ -21,10 +22,18 @@ public class Particles : MonoBehaviour {
     private ComputeBuffer particleBuffer;
 
     private void Start() {
+        dimension = Mathf.FloorToInt(Mathf.Sqrt(particleCount));
+        if (dimension < Mathf.Sqrt(particleCount))
+            particleCount -= 1;
+        
         particles = new Particle[particleCount];
         
-        for (int i = 0; i < particleCount; ++i) {
-            particles[i].position = Random.insideUnitSphere * radius;
+        for (int i = 0, z = 0; z < dimension; ++z) {
+            float v = (z + 0.5f) * (2.0f / particleCount) * radius;
+            for (int x = 0; x < dimension; ++x, ++i) {
+                float u = (x + 0.5f) * (2.0f / particleCount) * radius;
+                particles[i].position = new Vector3(u, 0, v);
+                }
         }
 
         particleBuffer = new ComputeBuffer(particleCount, PARTICLE_SIZE);
@@ -34,8 +43,12 @@ public class Particles : MonoBehaviour {
     }
 
     private void Update() {
-        for (int i = 0; i < particleCount; ++i) {
-            particles[i].position = Random.insideUnitSphere * radius;
+        for (int i = 0, z = 0; z < dimension; ++z) {
+            float v = (z + 0.5f) * (2.0f / particleCount) * radius;
+            for (int x = 0; x < dimension; ++x, ++i) {
+                float u = (x + 0.5f) * (2.0f / particleCount) * radius;
+                particles[i].position = new Vector3(u, 0, v);
+                }
         }
 
         particleBuffer.SetData(particles);
